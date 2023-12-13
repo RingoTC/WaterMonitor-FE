9e999e0292d63d8f458e411e0c985cb6337a34ca
vicshiyi <shiyivic@gmail.com>
2023-12-13 00:23:06 -0500

    reminder update

--- out/_next/static/chunks/app/page-6c55cf9a368ab8ad.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{7716:function(e,r,n){Promise.resolve().then(n.bind(n,4100))},4100:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return Home}});var t=n(7437);n(8995);var o=n(2265),u=n(4033);function Home(){let e=(0,u.useRouter)();return(0,o.useEffect)(()=>{e.push("/home")}),(0,t.jsx)("div",{children:"Loading..."})}},8995:function(){},622:function(e,r,n){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var t=n(2265),o=Symbol.for("react.element"),u=Symbol.for("react.fragment"),f=Object.prototype.hasOwnProperty,s=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,n){var t,u={},c=null,_=null;for(t in void 0!==n&&(c=""+n),void 0!==r.key&&(c=""+r.key),void 0!==r.ref&&(_=r.ref),r)f.call(r,t)&&!i.hasOwnProperty(t)&&(u[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===u[t]&&(u[t]=r[t]);return{$$typeof:o,type:e,key:c,ref:_,props:u,_owner:s.current}}r.Fragment=u,r.jsx=q,r.jsxs=q},7437:function(e,r,n){"use strict";e.exports=n(622)},4033:function(e,r,n){e.exports=n(290)}},function(e){e.O(0,[428,971,864,744],function(){return e(e.s=7716)}),_N_E=e.O()}]);
--- src/components/ticket_user_info/client.js
+++ src/components/ticket_user_info/client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const TICKETS_API = `${process.env.NEXT_PUBLIC_BACKEND}/record`;
+const USER_API = `${process.env.NEXT_PUBLIC_BACKEND}/user`;
 
 export const findTotalTickets = async () => {
     const response = await axios.get(`${TICKETS_API}/totalcount`);
@@ -14,6 +15,11 @@ export const findTotalComplete = async () => {
 
 export const findTotalLoading = async () => {
     const response = await axios.get(`${TICKETS_API}/totalLoading`);
+    return response.data;
+};
+
+export const updateUserReminder = async (username, reminder) => {
+    const response = await axios.put(`${USER_API}/update-reminder/${username}`, { reminder });
     return response.data;
 };
 
